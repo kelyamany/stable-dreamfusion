5e8264538aec4b74079aca6488877b1192f8d60e
Khaled Elyamany <khaled.elyamany@outlook.com>
2023-02-08 20:07:01 +0100

    recent changes

---
+++ scripts/install.sh
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+pip install -r requirements.optional.txt
+pip install -r requirements.extensions.txt
--- scripts/samples.sh
+++ scripts/samples.sh
@@ -2,6 +2,6 @@
 
 mkdir trials
 
-CUDA_VISIBLE_DEVICES=1 python main.py -O --text "a DSLR photo of a plywood chair" --workspace trials/chair
-CUDA_VISIBLE_DEVICES=1 python main.py -O --text "a DSLR photo of a plywood table" --workspace trials/table
-CUDA_VISIBLE_DEVICES=1 python main.py -O --text "a DSLR photo of a wooden house" --workspace trials/house
+CUDA_VISIBLE_DEVICES=1 python main.py -O --text "a simple plywood chair with planar surfaces" --workspace trials/chair --save_mesh --mcubes_resolution 128 --decimate_target 100 --num_steps 16 --max_steps 128 --albedo_iters 500 --density_thresh 1 --blob_density 1
+CUDA_VISIBLE_DEVICES=1 python main.py -O --text "a simple plywood table with planar surfaces" --workspace trials/table --save_mesh --mcubes_resolution 128 --decimate_target 100 --num_steps 16 --max_steps 128 --albedo_iters 500 --density_thresh 1 --blob_density 1
+CUDA_VISIBLE_DEVICES=1 python main.py -O --text "a simple plywood house with planar surfaces" --workspace trials/house --save_mesh --mcubes_resolution 128 --decimate_target 100 --num_steps 16 --max_steps 128 --albedo_iters 500 --density_thresh 1 --blob_density 1
